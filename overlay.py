4c4a0902deeb4e1df49804b68202548ad190d799
blazejmanczak <manczakb@gmail.com>
2020-11-26 09:57:35 +0100

    Added license stuff

--- overlay.py
+++ overlay.py
@@ -1,3 +1,18 @@
+# Copyright 2020-present, Netherlands Institute for Sound and Vision (Blazej Manczak)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
 from matchingMethods import all_in_one
 import argparse
 import os
@@ -53,7 +68,7 @@ if __name__ == "__main__":
     parser.add_argument("--save_dir",
                         required=False,
                         type=str,
-                        default="frames/sample_outputs",
+                        default="frames/outputs",
                         help="Directory where the overlayed images should be stored.")
 
     parser.add_argument("--data_directory",
